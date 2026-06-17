ddbb12cc8cf7928135824cc2f61e71397c28067a
chirag-x <chirag.sharma.gov2005@gmail.com>
2026-06-17 12:11:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.post("/chat", async (req, res) => {
     const response = await axios.post(
       "https://openrouter.ai/api/v1/chat/completions",
       {
-         model: "nvidia/nemotron-3-ultra-550b-a55b:free",
+        model: "openai/gpt-oss-120b:free",
         max_tokens: 300,
         temperature: 0.7,
         messages: [
